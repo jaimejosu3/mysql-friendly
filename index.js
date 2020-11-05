6ae171776bef16b17adef013512ff2b458522f63
Jaime Josue <jfonseca@MacBook-Pro-de-Jaime.local>
2020-11-05 16:34:45 -0600

    asd

--- index.js
+++ index.js
@@ -5,8 +5,7 @@ let connection = mysql.createConnection({
 	host: process.env.DB_HOST,
 	user: process.env.DB_USER,
 	password: process.env.DB_PASSWORD,
-	database: process.env.DB_NAME,
-	charset:"utf8mb4"
+	database: process.env.DB_NAME
 });
  
 connection.connect(function(err) {
