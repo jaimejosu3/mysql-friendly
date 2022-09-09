34dbaa93163b63d79b28ef5162ccbf364c0637e3
Jaime <jaime@cresven.com>
2022-09-08 20:36:27 -0600

    test reconnection

--- index.js
+++ index.js
@@ -1,20 +1,35 @@
 let mysql      = require('mysql');
 require('dotenv').config()
+let connection;
 
-let connection = mysql.createConnection({
-	host: process.env.DB_HOST,
-	user: process.env.DB_USER,
-	password: process.env.DB_PASSWORD,
-	database: process.env.DB_NAME,
-	charset:'utf8mb4'
-});
- 
-connection.connect(function(err) {
-	if (err) {
-		console.error('Error connecting: ' + err.stack);
-		return;
-	}
-});
+function handleError () {
+	connection = mysql.createConnection({
+		host: process.env.DB_HOST,
+		user: process.env.DB_USER,
+		password: process.env.DB_PASSWORD,
+		database: process.env.DB_NAME,
+		charset:'utf8mb4'
+	});
+    // Connection error, 2 seconds retry
+    connection.connect(function (err) {
+        if (err) {
+            console.log('error when connecting to db:', err);
+            setTimeout(handleError , 2000);
+        }
+    });
+
+    connection.on('error', function (err) {
+        console.log('db error', err);
+        // If the connection is disconnected, automatically reconnect
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+            handleError();
+        } else {
+            throw err;
+        }
+    });
+}
+
+handleError();
 
 const checkModels = () => {
 	try{
